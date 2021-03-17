6299a05f7a68789b0d1625429c3680becceb341d
jh Lim <jhwlim25@gmail.com>
2021-03-17 19:14:40 +0900

    블로그 설정 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "첫번째 글입니다.",
-        "excerpt":"웹 개발자가 되기 위해 공부한 내용을 정리하는 블로그입니다.   첫번째 주제  1-1  1-1-1  1-1-2  1-1-3  1-2   두번째 주제  2-1-3  ","categories": ["java"],
-        "tags": ["java","jsp","spring"],
-        "url": "https://jhwlim.github.io/java/first-post/",
+        "title": "Spring MVC 프로젝트 생성 및 초기설정",
+        "excerpt":"1. Spring MVC 프로젝트 생성하기 Eclipse 상단의 [File]-[New]-[Spring Legacy Project] 클릭 프로젝트 이름을 작성하고, Spring MVC Project 선택한 뒤 Next 프로젝트 패키지 경로를 작성한 뒤, Finish (이때, 패키지 경로는 3단계로, 도메인주소를 거꾸로 작성한다.) 2. 프로젝트 구조 SpringPractice : 프로젝트 이름 ├ src/main/java : 작성한 자바 파일들을 폴더 ├ src/main/resources :...","categories": ["spring"],
+        "tags": ["spring","spring-mvc","setting","pom.xml"],
+        "url": "https://jhwlim.github.io/spring/spring-start/",
+        "teaser": null
+      },{
+        "title": "Spring MVC 프로젝트 살펴보기",
+        "excerpt":"1. 들어가기 전에 Spring MVC 프로젝트를 생성하고, pom.xml 초기설정을 마친 후, 프로젝트가 실행되는 결과를 바탕으로 프로젝트가 동작되는 방식을 살펴보자. 2. 프로젝트 구조 프로젝트를 생성하면 아래와 같은 구조의 프로젝트가 생성된다. 이때, WEB-INF 폴더 아래의 파일에는 보안 때문에 직접 접근할 수 없다. WEB-INF/views/home.jsp 에서 Ctrl + F11 단축키를 통해 강제로 실행시켜도 404...","categories": ["spring"],
+        "tags": ["spring","spring-mvc","context-path"],
+        "url": "https://jhwlim.github.io/spring/spring-basic-1/",
         "teaser": null
       }]
